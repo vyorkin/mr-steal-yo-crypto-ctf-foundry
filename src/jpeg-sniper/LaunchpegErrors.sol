2fb3e55833a8d944d3f6ab11a5104baee92429fc
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2022-12-31 11:47:55 +0300

    Add level 1

---
+++ src/jpeg-sniper/LaunchpegErrors.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+error Launchpeg__CanNotMintThisMany();
+error Launchpeg__MaxSupplyReached();
+error Launchpeg__NotEnoughFunds(uint256 fundsSent);
+error Launchpeg__TransferFailed();
+error Launchpeg__Unauthorized();
+error Launchpeg__WrongPhase();
