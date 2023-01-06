774edfa9d06ae0e38362416aaca65c04d0276813
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-01-06 20:10:11 +0300

    Add WETH9

--- test/free-lunch/SafuMakerV2.t.sol
+++ test/free-lunch/SafuMakerV2.t.sol
@@ -7,12 +7,14 @@ import {console2} from "forge-std/console2.sol";
 import {Utilities} from "../Utilities.sol";
 import {Token} from "../../src/Token.sol";
 import {SafuMakerV2} from "../../src/free-lunch/SafuMakerV2.sol";
+import {WETH9} from "../../src/WETH9.sol";
 
 contract SafuMakerV2Test is Test {
   Utilities private utils;
   address payable private attacker;
 
   Token private usdc;
+  WETH9 private weth;
   SafuMakerV2 private safeMakerV2;
 
   function setUp() public {
@@ -21,11 +23,18 @@ contract SafuMakerV2Test is Test {
     address payable[] memory users = utils.createUsers(1);
     attacker = users[0];
     vm.label(attacker, "Attacker");
+
+    weth = new WETH9();
   }
 
   function testExploit() public {
     // Exploit start
     vm.startPrank(attacker);
+
+    usdc = new Token("USDC", "USDC");
+    usdc.mint(attacker, 100);
+    usdc.mint(address(this), 1000000);
+
     vm.stopPrank();
     // Exploit end
     validate();
